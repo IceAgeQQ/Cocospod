4afb05c8239c3bcd368b926acab15f3721494216
IceAgeQQ <d830300042@gmail.com>
2014-06-22 22:47:37 -0700

    Second Commit

--- macyAssignment/AppDelegate.h
+++ macyAssignment/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Chao Xu on 14-2-4.
 //  Copyright (c) 2014年 Chao Xu. All rights reserved.
 //
-
+//hello world
 #import <UIKit/UIKit.h>
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
